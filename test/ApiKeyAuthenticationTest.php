ade3f2ac5823496f641844c5b9a8f07dc42f52e1
matt <matt@claritum.com>
2026-02-15 14:14:51 +0000

    Bump PHPUnit and Psalm, fix PHPUnit deprecations
    # Conflicts:
    #	composer.lock

--- test/ApiKeyAuthenticationTest.php
+++ test/ApiKeyAuthenticationTest.php
@@ -20,23 +20,14 @@ use Psr\Http\Message\ServerRequestInterface;
 final class ApiKeyAuthenticationTest extends TestCase
 {
     private RequestParserInterface&Stub $requestParser;
-    private UserRepositoryInterface&Stub $userRepository;
     private ResponseFactoryInterface&Stub $responseFactory;
-    private ApiKeyAuthentication $authentication;
 
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->requestParser   = self::createStub(RequestParserInterface::class);
-        $this->userRepository  = self::createStub(UserRepositoryInterface::class);
         $this->responseFactory = self::createStub(ResponseFactoryInterface::class);
-
-        $this->authentication = new ApiKeyAuthentication(
-            $this->requestParser,
-            $this->userRepository,
-            $this->responseFactory
-        );
     }
 
     public function testAuthenticateMissingKeyDoesNotAuthenticateAgainstUserRepository(): void
@@ -65,11 +56,19 @@ final class ApiKeyAuthenticationTest extends TestCase
         $apiKey     = new ApiKey('foo', $identifier, $secret);
         $this->requestParser->method('getApiKey')
             ->willReturn($apiKey);
-        $this->userRepository->method('authenticate')
+        $userRepository = $this->createMock(UserRepositoryInterface::class);
+        $userRepository->expects(self::once())
+            ->method('authenticate')
             ->with($identifier, $secret)
             ->willReturn($expected);
 
-        $actual = $this->authentication->authenticate(self::createStub(ServerRequestInterface::class));
+        $authentication = new ApiKeyAuthentication(
+            $this->requestParser,
+            $userRepository,
+            $this->responseFactory
+        );
+
+        $actual = $authentication->authenticate(self::createStub(ServerRequestInterface::class));
         self::assertSame($expected, $actual);
     }
 
@@ -77,10 +76,16 @@ final class ApiKeyAuthenticationTest extends TestCase
     {
         $expected = self::createStub(ResponseInterface::class);
         $this->responseFactory->method('createResponse')
-            ->with(401)
             ->willReturn($expected);
+        $userRepository = self::createStub(UserRepositoryInterface::class);
 
-        $actual = $this->authentication->unauthorizedResponse(self::createStub(ServerRequestInterface::class));
+        $authentication = new ApiKeyAuthentication(
+            $this->requestParser,
+            $userRepository,
+            $this->responseFactory
+        );
+
+        $actual = $authentication->unauthorizedResponse(self::createStub(ServerRequestInterface::class));
         self::assertSame($expected, $actual);
     }
 }
